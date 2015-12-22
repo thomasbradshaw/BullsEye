61ca67e69d97a50fdcaffbd28187185dadb6871f
thomasbradshaw <tb@tbmacbookpro.local>
2015-12-22 17:14:58 -0600

    iOS - Added resizable views - icons.

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import QuartzCore
 
 class ViewController: UIViewController {
   
@@ -164,7 +165,13 @@ class ViewController: UIViewController {
   
   @IBAction func startOver() {
     startNewGame()
+    
+    // Do a cool transition - crossfade
+    let transition = CATransition()
+    transition.type = kCATransitionFade
+    transition.duration = 1
+    transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+    view.layer.addAnimation(transition, forKey: nil)
   }
-
 }
 
